471fc0ef6e4f272131f98bcb30ebaa47197d69ec
mastachii <mastachii273@gmail.com>
2025-01-30 16:33:58 +0800

    wip: Populating db

--- model/queries.js
+++ model/queries.js
@@ -3,26 +3,47 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 class Level {
-    async createLevel({ name, sceneName, waldo, wenda, woof, odlaw, wizard }) {
+    async createLevel({ name, sceneName, waldo, wenda, wilma, woof, odlaw, wizard }) {
         await prisma.level.create({
-            name,
-            sceneName,
-            waldo,
-            wenda,
-            woof,
-            odlaw,
-            wizard,
+            data: {
+                name,
+                sceneName,
+                waldo,
+                wenda,
+                wilma,
+                woof,
+                odlaw,
+                wizard,
+            },
         });
+    }
+
+    async getAllLevels() {
+        const levels = await prisma.level.findMany();
+        console.log(levels);
+        return levels;
     }
 }
 
 const db = new Level();
 
-db.createLevel({
-    name: "hollywood",
-    sceneName: "When the Stars Come Out",
-    waldo: "1350, 532",
-    woof: "1635, 805",
-    wenda: "1136, 832",
-    odlaw: "1066, 986",
-});
+// db.createLevel({
+//     name: "hollywood",
+//     sceneName: "When the Stars Come Out",
+//     waldo: "1350, 532",
+//     woof: "1635, 805",
+//     wilma: "1136, 832",
+//     odlaw: "1066, 986",
+// });
+
+// db.createLevel({
+//     name: "fruit",
+//     sceneName: "The Might Fruit Fight",
+//     waldo: "1712, 716",
+//     wilma: "256, 917",
+//     woof: "1684, 615",
+//     odlaw: "1269, 605",
+//     wizard: "483, 527",
+// });
+
+db.getAllLevels();
